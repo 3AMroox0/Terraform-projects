222bcccbbee025d748a0738d08d93558cec089f3
3AMroox0 <amr.sulaiman1@gmail.com>
2022-02-09 07:12:10 -0800

    usage of provisioners

--- First-Project/main.tf
+++ First-Project/main.tf
@@ -9,6 +9,7 @@ variable env_prefix {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_location" {}
+variable "private_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -122,7 +123,27 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = file("entry-script.sh")
+    #user_data = file("entry-script.sh")
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ubuntu"
+        private_key =  file(var.private_key_location)
+    }
+
+    provisioner "file" {
+        source = "entry-script.sh"
+        destination = "home/ubuntu/entry-script.sh"
+    }
+
+    provisioner "remote-exec" {
+        script = file("entry-script.sh")
+    }
+
+    provisioner "local-exec" {
+        command = "echo ${self.public_ip} > output-ip.txt"
+    }
 
     tags = {
         Name: "${var.env_prefix}-server"
